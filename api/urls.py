9b227f937518530d15ef1826c31847d57e4eb227
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-02-24 12:01:13 +0530

    Implemented rest_fraework serializers and GET api_view decorator

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from . import views
 from django.urls import path
 
 urlpatterns=[
-    path('students/',views.Studentview),
+    path('students/',views.Studentsview),
     
 ]
--- api/views.py
+++ api/views.py
@@ -1,8 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from students.models import Student
-# Create your views here.
-def Studentview(request):
-    students=Student.objects.all()
-    students_list=list(students.values())
-    return JsonResponse(students_list, safe=False)
+from .serializers import StudentSerializer
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.decorators import api_view
+
+@api_view(['GET'])
+def Studentsview(request):
+    if(request.method=='GET'):
+        students=Student.objects.all()
+        serializer=StudentSerializer(students,many=True)
+        return Response(serializer.data,status=status.HTTP_200_OK)
