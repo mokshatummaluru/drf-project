5439b91397c8c42e528ddee07f329a3139121754
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-02-24 12:11:28 +0530

    Implemented POST API request on API Endpoint

--- api/views.py
+++ api/views.py
@@ -6,9 +6,15 @@ from rest_framework.response import Response
 from rest_framework import status
 from rest_framework.decorators import api_view
 
-@api_view(['GET'])
+@api_view(['GET', 'POST'])
 def Studentsview(request):
     if(request.method=='GET'):
         students=Student.objects.all()
         serializer=StudentSerializer(students,many=True)
         return Response(serializer.data,status=status.HTTP_200_OK)
+    elif(request.method=='POST'):
+        serializer=StudentSerializer(data=request.data)
+        if(serializer.is_valid()):
+            serializer.save()
+            return Response(serializer.data,status=status.HTTP_201_CREATED)
+        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
