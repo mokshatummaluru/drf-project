772a3b689a1259f1c697dc30c5002a9951d172dc
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-02-24 14:17:06 +0530

    Implemented class based views and understood the difference between function based views and class based views

--- api/urls.py
+++ api/urls.py
@@ -4,4 +4,7 @@ from django.urls import path
 urlpatterns=[
     path('students/',views.Studentsview),
     path('students/<int:pk>/',views.studentDetailView),
+    path('employees/',views.Employees.as_view()),
+    path('employees/<int:pk>/',views.EmployeeDetail.as_view()),
+    
 ]
