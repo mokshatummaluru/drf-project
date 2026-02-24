ab614d44321d9ed394e4b8dbbca70e7fb0f144dd
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-02-24 11:38:32 +0530

    Created a Model and Implemented manual serialisation to return JsonResponse

--- api/views.py
+++ api/views.py
@@ -1,10 +1,8 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from students.models import Student
 # Create your views here.
 def Studentview(request):
-    students={
-        "id":12,
-        "name":"Moksha",
-        "age":22,
-    }
-    return JsonResponse(students)
+    students=Student.objects.all()
+    students_list=list(students.values())
+    return JsonResponse(students_list, safe=False)
