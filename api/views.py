b1dd078cc2e77b59b2803798255e4fe4834e61ee
MokshaTummaluru <112241855+mokshatummaluru@users.noreply.github.com>
2026-02-24 15:12:23 +0530

    Implemented ModelViewsets

--- api/views.py
+++ api/views.py
@@ -128,21 +128,6 @@ def studentDetailView(request,pk):
 #     lookup_field='pk'
 
 
-class EmployeeViewset(viewsets.ViewSet):
-    def list(self,request):
-        queryset=Employee.objects.all()
-        serializer=EmployeeSerializer(queryset,many=True)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
-    def create(self,request):
-        serializer=EmployeeSerializer(data=request.data)
-        if(serializer.is_valid()):
-            serializer.save()
-            return Response(serializer.data,status=status.HTTP_201_CREATED)
-        return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
-    
-    def retrieve(self,request,pk=None):
-        employee=Employee.get_object_or_404(Employee,pk=pk)
-        serializer=EmployeeSerializer(employee)
-        return Response(serializer.data,status=status.HTTP_200_OK)
-    
+class EmployeeViewset(viewsets.ModelViewSet):
+    queryset=Employee.objects.all()
+    serializer_class=EmployeeSerializer
